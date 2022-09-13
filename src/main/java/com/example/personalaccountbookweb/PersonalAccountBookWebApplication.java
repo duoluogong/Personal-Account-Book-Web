70d1565b039442cea0174fe13938589c9ff0c354
unknown <1950790043@qq.com>
2022-09-13 15:58:13 +0800

    first commit

---
+++ src/main/java/com/example/personalaccountbookweb/PersonalAccountBookWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalaccountbookweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAccountBookWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalAccountBookWebApplication.class, args);
+    }
+
+}
